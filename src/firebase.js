f292c7510f0a838a239e476938e8938f886de0db
Sagar Kaushik <sagarkaushik584@gmail.com>
2025-08-23 13:43:10 +0530

    Update firebase.js
    updated comments

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,3 @@
-// import { initializeApp } from "firebase/app";
-
-// import { getDatabase } from "firebase/database";
-//  // or wherever your firebase config is
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDoTFQROyMkCJwhshWGibgCwRSZcUaZEnM",
-//   authDomain: "portfolio-c2dd4.firebaseapp.com",
-//   projectId: "portfolio-c2dd4",
-//   storageBucket: "portfolio-c2dd4.firebasestorage.app",
-//   messagingSenderId: "662391806907",
-//   appId: "1:662391806907:web:c7dbb76c5b7132b9a5829e",
-//   measurementId: "G-MGMR21ZBC1",
-//  databaseURL:"https://portfolio-c2dd4-default-rtdb.firebaseio.com/",
-// };
-
-// export const app = initializeApp(firebaseConfig);
-
-
-// export const database = getDatabase(app);
-
 
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
@@ -42,10 +20,11 @@ export const app = initializeApp(firebaseConfig);
 // Database instance
 export const database = getDatabase(app);
 
-// ✅ Auth instance
+// Auth instance
 export const auth = getAuth(app);
 
-// ✅ Sign in anonymously (so rules can check auth != null)
+// Sign in anonymously (so rules can check auth != null)
 signInAnonymously(auth).catch((error) => {
   console.error("Auth error:", error);
 });
+
